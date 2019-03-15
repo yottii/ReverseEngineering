78f97020c140359d6f3d4656461fc43e56e64089
ykusama@keio.jp <ykusama@keio.jp>
2019-03-15 17:00:12 +0900

    updata

--- frida-iOS/v3/get_method.py
+++ frida-iOS/v3/get_method.py
@@ -19,32 +19,26 @@ def on_message(message, data):
         print(e)
 
 def do_hook():
-    hook = """
-        if(ObjC.available) {
+    get_method_information = """
+    console.log("[*]Start");
+    if(ObjC.available) {
         var class_checker = ObjC.classes.JailbreakDetection;
         var methods_checker = class_checker.$ownMethods;
         methods_checker.forEach(function(m) {
-            send(m);
-        });
-    } else {
-        console.log("Objective-C Runtime is not available!");
-    }
+                send(m);
+                });
+        } else {
+        console.log("frida is not availabe on your devices");
+        }
+    console.log("[*]End");
 """
-    return hook
+    return get_method_information
 
 if __name__ == '__main__' :
     PACKAGE_NAME = "JailbreakDetection_Objective-C"
     try :
         
         session = frida.get_usb_device().attach(PACKAGE_NAME)
-        #print ("[log] devices info : {}".format(frida.get_device_manager().enumerate_devices()))
-        #device = frida.get_device_manager().enumerate_devices()[-1]
-        #pid = device.spawn([PACKAGE_NAME])
-        #print ("[log] {} is starting. (pid : {})".format(PACKAGE_NAME, pid))
-        #session = frida.get_usb_device().attach(PACKAGE_NAME)
-        #session = device.attach(pid)
-        #device.resume(pid)
-
         script = session.create_script(do_hook())
         script.on('message', on_message)
         script.load()
